dbfd6fa0efea97568c96348589d6c5ae19bd2efe
Stanimir0 <futboolgod2018@gmail.com>
2023-02-22 18:21:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.hamburger-menu').click(function(){
+        $('.sliding-navbar').toggleClass('sliding-navbar--open');
+        $('.mask').toggleClass('show')
+        $('.hamburger').toggleClass('menu-opened');
+    });
+
+    $('.mask').click(function(){
+        $('.sliding-navbar').toggleClass('sliding-navbar--open');
+        $('.mask').removeClass('show');
+        $('.hamburger').toggleClass('menu-opened');
+    })
+});
